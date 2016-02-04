c8a05332b389e165a7ce479213ad80d866dd8bc7
Jonathon Bellew <jonathonbellew@Jonathons-MacBook-Air.local>
2016-02-04 15:18:57 +1100

    Admin section

---
+++ app/controllers/landing_controller.rb
@@ -0,0 +1,7 @@
+class LandingController < ApplicationController
+
+  def admin
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,10 @@ Rails.application.routes.draw do
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
   get 'logout' => 'sessions#destroy'
+
+  # Admin
+  scope :admin do
+    get '' => 'landing#admin', as: :admin
+
+  end
 end
