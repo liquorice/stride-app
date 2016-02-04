e284c10add6bd6b875066a52ca3d3f2251d85884
Jonathon Bellew <jonathonbellew@Jonathons-MacBook-Air.local>
2016-02-04 15:17:05 +1100

    Basic user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # Manage session and user log-ins
+  include SessionsHelper
+  before_action :check_for_user
+
+  private
+
+  def check_for_user
+    logged_in?
+  end
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,33 @@
+class SessionsController < ApplicationController
+
+  def new
+    return redirect_to :root if logged_in?
+
+    render :login
+  end
+
+  def create
+    user = User.find_by(name: params[:session][:name])
+    failed unless user
+
+    if user.authenticate(params[:session][:password])
+      log_in(user)
+      redirect_to :root
+    else
+      failed
+    end
+  end
+
+  def destroy
+    log_out
+    redirect_to :root
+  end
+
+  private
+
+  def failed
+    flash.now[:error] = 'Invalid name/password combination' # Not quite right!
+    render :login
+  end
+
+end
