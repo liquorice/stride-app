c1f7a3ee65dc69186cceca5d7ed503ea2985509c
Jonathon Bellew <jonathonbellew@Jonathons-MacBook-Air.local>
2016-03-04 19:48:15 +0100

    Fix redirection on failed login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(name: params[:session][:name])
-    failed unless user
+    return failed unless user
 
     if user.authenticate(params[:session][:password])
       log_in(user)
