f63b5ae3936acee1316b2fc09a02ac25e4d3c967
Jonathon Bellew <jonathonbellew@Jonathons-MacBook-Air.local>
2016-03-07 16:10:45 +0100

    Add jquery-rails, fix application.html.slim, make logout POST
    Logout is a destructive action, and as such should not be available with GET.
    This is the first non-GET link, so needed to add jquery-rails.
    In adding application.js, I fixed the indentation issues on application.html.slim

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # Session management
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
-  get 'logout' => 'sessions#destroy'
+  post 'logout' => 'sessions#destroy'
 
   # Admin
   scope :admin do
