f1d8e8d4b12a8ad737e23e4813d57ebe5d20ba47
Michelle Chan <michelle@liquoricestudio.com>
2016-04-11 18:56:35 +1000

    - implemented footer section - fleshing out topic thread styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,14 @@ module ApplicationHelper
   def insert_svg filename, options={}
     file = File.read(Rails.root.join('app', 'assets', 'images', filename))
     doc = Nokogiri::HTML::DocumentFragment.parse file
-    svg = doc.at_css 'svg'
+    svg = doc.at_css "svg"
     if options[:class].present?
-      svg['class'] = options[:class]
+      svg["class"] = options[:class]
     end
-    doc.to_html.html_safe
+    raw doc
   end
+
+  # def inline_svg(path)
+  #   raw Rails.application.assets.find_asset(path).to_s
+  # end
 end
