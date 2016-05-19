f560c0d5498a22ad0d5812c376bb973b2b6e73f6
Jonathon Bellew <jonathonbellew@Jonathons-MacBook-Air.local>
2016-05-19 14:44:41 +0200

    Add ability to quote posts
    Clicking the "quote" button copies the posts id to a hidden field "quoted_post_id", which is used to save a reference to the quoted post in the post being made.
    The ID is validated to make sure it is a post that exists in the same thread, and that hasn't been hidden by a moderator.
    A display above the post entry box indicates a brief description of the post being quoted, along with a button to remove the quotation.
    The visibility of the entire quoted section is controlled with a class "active".
    Posts render quoted posts with the same partial, but with an added parameter "being_quoted", to stop recursion and to make sure controls for quoting, moderation etc don't appear within the quoted post. The entire quoted post is within .post__quoted, which can be targeted to provide an alternate styling.

--- app/assets/javascripts/post-reply.js
+++ app/assets/javascripts/post-reply.js
@@ -3,15 +3,30 @@ Module.register('postReply', function(_container) {
 
   var textarea;
   var container;
+  var quoted_container;
+  var quoted_input;
+  var quoted_display;
 
   var init = function() {
     container = _container;
-    textarea = _container.find('.js-textarea');
+    textarea = container.find('.js-textarea');
 
+    quoted_container = container.find('.js-quoted');
+    quoted_input = quoted_container.find('.js-quoted-post-id');
+    quoted_display = quoted_container.find('.js-quoted-display');
+
+    setup_clear();
+    setup_growing_textarea();
+    setup_quoting();
+  }
+
+  var setup_clear = function() {
     container.find('.js-clear').on('click', function() {
       textarea.val('').trigger('input');
     });
+  }
 
+  var setup_growing_textarea = function() {
     textarea.on('input', function() {
       if (textarea.val().length) {
         container.addClass('hasContent');
@@ -26,7 +41,31 @@ Module.register('postReply', function(_container) {
     });
 
     textarea.trigger('input');
+  }
 
+  var setup_quoting = function() {
+    var quote_buttons;
+
+    quote_buttons = $('.js-post-quote');
+
+    quote_buttons.on('click', function() {
+      var quote_button;
+      quote_button = $(this);
+
+      quoted_container.addClass('active');
+      quoted_input.val(quote_button.attr('data-post-id'));
+      quoted_display.text(quote_button.attr('data-post-description'));
+    });
+
+    quoted_container.find('.js-quoted-clear').on('click', clear_quote);
+
+    clear_quote();
+  }
+
+  var clear_quote = function() {
+    quoted_container.removeClass('active');
+    quoted_input.val('');
+    quoted_display.text('');
   }
 
   init();
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160519103345) do
+ActiveRecord::Schema.define(version: 20160519120824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20160519103345) do
     t.integer  "user_id"
     t.boolean  "visible",         default: true
     t.datetime "hidden_at"
+    t.integer  "quoted_post_id"
   end
 
   add_index "posts", ["topic_thread_id"], name: "index_posts_on_topic_thread_id", using: :btree
