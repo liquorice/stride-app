ea0788492c2bac2196b8d59984634e1c0aa46d26
Michelle Chan <michelle@liquoricestudio.com>
2016-05-24 15:20:39 +1000

    Created dismiss button style and function for notifications

--- app/assets/javascripts/notification.js
+++ app/assets/javascripts/notification.js
@@ -1,6 +1,18 @@
 Module.register('notification', function(_container) {
   'use strict';
 
+  var close;
+
+  var init = function() {
+    close = _container.find('.js-notification-close');
+
+    close.on('click', function() {
+      _container.addClass('hidden');
+    });
+  };
+
+  init();
+
   setTimeout(function() {
     _container.addClass('hidden');
   }, 5000);
