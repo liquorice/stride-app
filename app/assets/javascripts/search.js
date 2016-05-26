ab75d60df5be2c64c122470d102d844c876be6fa
Michelle Chan <michelle@liquoricestudio.com>
2016-05-26 15:52:19 +1000

    - Resaved search svg icon - Updated search js to allow for width toggle on smaller breakpoints

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -3,15 +3,21 @@ Module.register('search', function(_container) {
 
   var form;
   var input;
+  var toggle;
 
   var init = function() {
-    form = _container;
+    form = _container.find('.js-search-form');
     input = form.find('.js-search-input');
+    toggle = form.find('.js-search-toggle');
 
     form.on('submit', function(e) {
       document.location.href = form.attr('action') + '/' + encodeURIComponent(input.val());
       e.preventDefault();
       return false;
+    });
+
+    toggle.on('click', function() {
+      _container.toggleClass('expand');
     });
   }
 
