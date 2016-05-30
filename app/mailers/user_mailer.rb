0b9147b5423c9865af5499cb60c922e56c7b3590
Jonathon Bellew <jonathonbellew@Jonathons-MacBook-Air.local>
2016-05-30 17:11:44 +0200

    Send reset emails to user emails
    This is in anticipation of having AWS approve our SES email account

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,9 +4,8 @@ class UserMailer < ApplicationMailer
     @host = host
     @user = user
     @reset_url = reset_url
-    # to = @user.email
-    # TODO change this to the users email once Amazon SES sending is approved
-    mail(to: 'dev@liquoricestudio.com', subject: 'Password reset request')
+    to = @user.email
+    mail(to: to, subject: 'Password reset request')
   end
 
 end
