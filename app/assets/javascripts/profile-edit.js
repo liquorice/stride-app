200cd782f8daa6c40700510459dc44d1345158e2
Michelle Chan <michelle@liquoricestudio.com>
2016-05-30 12:37:45 +1000

    - Updated Profile Javascript to preview display user avatar upon clicking face/colour options - Cancel button returns avatar to original (since it hasn't been saved) - Added js classes to Preference view - Added transitions to avatar scss

--- app/assets/javascripts/profile-edit.js
+++ app/assets/javascripts/profile-edit.js
@@ -2,18 +2,36 @@ Module.register('profileEdit', function(_container) {
   'use strict';
 
   var container;
-  var content;
   var cancel;
+  var avatar;
+  var userFace;
+  var userColour;
 
   var init = function() {
     container = _container;
     cancel = container.find('.js-profile-edit');
+    avatar = container.find('.js-profile-avatar').find('.avatar')[0];
+    if (avatar) {
+      userFace = avatar.getAttribute('data-face');
+      userColour = avatar.getAttribute('data-colour');
+    }
 
     cancel.on('click', function() {
+      if (avatar) {
+        avatar.setAttribute('data-face', userFace);
+        avatar.setAttribute('data-colour', userColour);
+      }
       container.toggleClass('edit');
     });
   }
 
   init();
 
+  container.find('.js-profile-face').on('click', function(selected) {
+    avatar.setAttribute('data-face', (selected.target.getAttribute('data-face')));
+  });
+
+  container.find('.js-profile-colour').on('click', function(selected) {
+    avatar.setAttribute('data-colour', (selected.target.getAttribute('data-colour')));
+  });
 });
