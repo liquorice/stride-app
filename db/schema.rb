7d2601bf0384f494282b951982d0706b4b3485af
Michelle Chan <michelle@liquoricestudio.com>
2016-06-27 11:37:53 +1000

    Migrated db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160613110820) do
+ActiveRecord::Schema.define(version: 20160624150951) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -129,10 +129,11 @@ ActiveRecord::Schema.define(version: 20160613110820) do
   create_table "topics", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
     t.integer  "site_id"
     t.integer  "ordinal",     default: 0
+    t.boolean  "visible",     default: true
   end
 
   add_index "topics", ["site_id"], name: "index_topics_on_site_id", using: :btree
