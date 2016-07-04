6be48c30f289e5990acfc4e65160468b20dbe55c
Jonathon Bellew <jonathonbellew@Jonathons-MacBook-Air.local>
2016-07-04 15:48:18 +0200

    Fix API connection to KB
    Also DRY-up AJAX request, so only one is made per page

--- app/assets/javascripts/api.js
+++ app/assets/javascripts/api.js
@@ -2,17 +2,20 @@
 
   'use strict';
 
-  var kb_url;
-  var api_url;
-
-  var survey_wrapper;
-  var get_support_wrapper;
-  var footer_wrapper;
 
   var init = function() {
+    $.get(
+      KB_API_URL,
+      process_api_response,
+      "JSON"
+    );
+  };
 
-    kb_url = 'http://' + document.domain.split('.').slice(1).join('.');
-    api_url = kb_url + '/api';
+
+  var process_api_response = function(data) {
+    var survey_wrapper;
+    var get_support_wrapper;
+    var footer_wrapper;
 
     var forums_overview_wrapper = $('.js-forums_overview');
     var chats_overview_wrapper = $('.js-chats_overview');
@@ -27,94 +30,75 @@
     get_support_wrapper = $('.js-get_support');
     footer_wrapper = $('.js-footer_wrapper');
 
-
     if (forums_overview_wrapper.length) {
-      $.get( api_url, function( data ) {
-        if (data["forums_overview"]) {
-          forums_overview_wrapper[0].innerHTML = data["forums_overview"];
-        }
-      }, "json");
+      if (data["forums_overview"]) {
+        forums_overview_wrapper[0].innerHTML = data["forums_overview"];
+      }
     }
 
     if (chats_overview_wrapper.length) {
-      $.get( api_url, function( data ) {
-        if (data["chats_overview"]) {
-          chats_overview_wrapper[0].innerHTML = data["chats_overview"];
-        }
-      }, "json");
+      if (data["chats_overview"]) {
+        chats_overview_wrapper[0].innerHTML = data["chats_overview"];
+      }
     }
 
     if (preferences_overview_wrapper.length) {
-      $.get( api_url, function( data ) {
-        if (data["preferences"]) {
-          preferences_overview_wrapper[0].innerHTML = data["preferences"]["overview"];
-        }
-      }, "json");
+      if (data["preferences"]) {
+        preferences_overview_wrapper[0].innerHTML = data["preferences"]["overview"];
+      }
     }
 
     if (preferences_notification_wrapper.length) {
-      $.get( api_url, function( data ) {
-        if (data["preferences"]) {
-          preferences_notification_wrapper[0].innerHTML = data["preferences"]["notifications_desc"];
-        }
-      }, "json");
+      if (data["preferences"]) {
+        preferences_notification_wrapper[0].innerHTML = data["preferences"]["notifications_desc"];
+      }
     }
 
     if (community_overview_wrapper.length) {
-      $.get( api_url, function( data ) {
-        if (data["community_rules"]) {
-          community_overview_wrapper[0].innerHTML = data["community_rules"]["overview"];
-        }
-      }, "json");
+      if (data["community_rules"]) {
+        community_overview_wrapper[0].innerHTML = data["community_rules"]["overview"];
+      }
     }
 
     if (community_rules_wrapper.length) {
-      $.get( api_url, function( data ) {
-        if (data["community_rules"]) {
-          community_rules_wrapper[0].innerHTML = data["community_rules"]["content"];
-        }
-      }, "json");
+      if (data["community_rules"]) {
+        community_rules_wrapper[0].innerHTML = data["community_rules"]["content"];
+      }
     }
 
     if (survey_wrapper.length) {
-      $.get( api_url, function( data ) {
-        if (data["survey"]) {
-          survey_wrapper.find('.js-survey_content')[0].innerHTML = data["survey"]["content"];
-          survey_wrapper.find('.js-survey_link')[0].setAttribute('href', data["survey"]["link"]);
-        }
-      }, "json");
+      if (data["survey"]) {
+        survey_wrapper.find('.js-survey_content')[0].innerHTML = data["survey"]["content"];
+        survey_wrapper.find('.js-survey_link')[0].setAttribute('href', data["survey"]["link"]);
+      }
     }
 
     if (get_support_wrapper.length) {
-      $.get( api_url, function( data ) {
-        if (data["footer"]) {
-          get_support_wrapper[0].innerHTML = data["footer"]["get_support"];
-        }
-      }, "json");
+      if (data["footer"]) {
+        get_support_wrapper[0].innerHTML = data["footer"]["get_support"];
+      }
     }
 
     if (footer_wrapper.length) {
-      $.get( api_url, function( data ) {
-        if (data["footer"]) {
-          var beyondblue_anchors = footer_wrapper.find('.js-beyondblue_link');
-          var movember_anchors = footer_wrapper.find('.js-movember_link');
+      if (data["footer"]) {
+        var beyondblue_anchors = footer_wrapper.find('.js-beyondblue_link');
+        var movember_anchors = footer_wrapper.find('.js-movember_link');
 
-          for (var i = 0; i < beyondblue_anchors.length; i++) {
-            beyondblue_anchors[i].setAttribute('href', data["footer"]["beyondblue"]);
-          }
-
-          for (var j = 0; j < movember_anchors.length; j++) {
-            movember_anchors[j].setAttribute('href', data["footer"]["movember"]);
-          }
-
-          footer_wrapper.find('.js-vac_link')[0].setAttribute('href', data["footer"]["vac"]);
-          footer_wrapper.find('.js-acon_link')[0].setAttribute('href', data["footer"]["acon"]);
-          footer_wrapper.find('.js-csrh_link')[0].setAttribute('href', data["footer"]["csrh"]);
-          footer_wrapper.find('.js-additional_link')[0].setAttribute('href', data["footer"]["additional"]);
+        for (var i = 0; i < beyondblue_anchors.length; i++) {
+          beyondblue_anchors[i].setAttribute('href', data["footer"]["beyondblue"]);
         }
-      }, "json");
+
+        for (var j = 0; j < movember_anchors.length; j++) {
+          movember_anchors[j].setAttribute('href', data["footer"]["movember"]);
+        }
+
+        footer_wrapper.find('.js-vac_link')[0].setAttribute('href', data["footer"]["vac"]);
+        footer_wrapper.find('.js-acon_link')[0].setAttribute('href', data["footer"]["acon"]);
+        footer_wrapper.find('.js-csrh_link')[0].setAttribute('href', data["footer"]["csrh"]);
+        footer_wrapper.find('.js-additional_link')[0].setAttribute('href', data["footer"]["additional"]);
+      }
     }
-  };
+  }
 
   $(init);
 
