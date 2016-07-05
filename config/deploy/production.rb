b3f2630ce995b049f7a5381438eb2217c15c7c2f
Jonathon Bellew <jonathonbellew@Jonathons-MacBook-Air.local>
2016-07-05 11:10:03 +0200

    Production settings

---
+++ config/deploy/production.rb
@@ -0,0 +1,10 @@
+server '52.63.26.76', user: 'ubuntu', roles: %w{web app}
+
+set :full_app_name, "#{fetch(:application)}-#{fetch(:stage)}"
+
+set :deploy_to, "/var/www/stride-production"
+
+set :unicorn_config_path, '/var/www/stride-production/current/config/unicorn/production.rb'
+set :unicorn_pid, '/var/www/stride-production/shared/tmp/unicorn-production.pid'
+
+set :branch, 'production'
