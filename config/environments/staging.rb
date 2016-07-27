a4869184fde95e548028c0165f8d65efd597692a
Jonathon Bellew <jonathonbellew@Jonathons-MacBook-Air.local>
2016-07-27 11:55:02 +0200

    Add app_path to production and staging sites

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -3,6 +3,7 @@ Rails.application.configure do
 
   # The % will be replaced by the site name
   config.kb_path = 'http://stage.%.org.au'
+  config.app_path = 'http://forums.stage.%-app.dev'
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
