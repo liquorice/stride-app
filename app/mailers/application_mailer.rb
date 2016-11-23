27c545a52dbcc8c7992ce80451f31e4d4e25bb13
Michelle Chan <michelle@liquoricestudio.com>
2016-11-23 17:13:24 +1100

    Debugging on production, problem with action mailer. Testing with Password Request instead of Notifications so other users don't get test emails.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,6 +2,6 @@ class ApplicationMailer < ActionMailer::Base
   helper MailerHelper
 
   # TODO: Change to correct address
-  default from: 'dev@liquoricestudio.com'
+  # default from: 'dev@liquoricestudio.com'
   layout 'mailer'
 end
